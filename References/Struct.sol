5d709361c6a9d58f071ea9f4dd5293467c0d49ac
SenatorGeeky <119044720+SenatorGeeky@users.noreply.github.com>
2022-11-25 16:15:50 +0530

    creating struct

---
+++ References/Struct.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.8;
+
+
+contract strcutTest{
+
+    struct Flowers{
+        string name;
+        uint256 price;
+        string colour;
+        
+     }
+  Flowers public var1 = Flowers("rose",20,"white");
+  string public var2= "hello";
+    }
